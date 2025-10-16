96fe6bbe5180ab8351e9775fe2b73fa9314d1269
anduong <duongnka219@gmail.com>
2025-10-16 13:53:01 +0700

    add consume data from kafka to cassandra using structured streaming

--- spark-submit/script.sh
+++ spark-submit/script.sh
@@ -1,1 +1,3 @@
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.6 /home/iceberg/notebooks/notebooks/kafka_to_iceberg.py
+
+spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.6,com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 /home/iceberg/notebooks/notebooks/kafka_to_cassandra.py
